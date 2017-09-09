6bdad277d9f52e47d23b7f8ecfb5645b19b88d72
jared-jackson <lanejackson4@hotmail.com>
2017-09-09 15:42:20 -0500

    consultWatson code changes

--- index.js
+++ index.js
@@ -9,36 +9,30 @@ api.get('/', function () {
 
     var test_string = "<div><h3>THIS IS A SUPER AWESOME STRING WE ARE GOING TO BE EXAMINING</h3>";
 
-    var test = consultWatson(test_string);
-
-    return test + "THIS";
-    //return test_string;
-});
-
-
-function consultWatson(string) {
     var nlu = new NaturalLanguageUnderstandingV1({
         username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
         password: 'JBAtZWNXWqy6',
         version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
     });
 
-    return string;
+    nlu.analyze({
+        'html': test_string, // Buffer or String
+        'features': {
+            'concepts': {},
+            'keywords': {},
+            'emotion': {}
+        }
+    }, function(err, response) {
+        var test = response.emotion;
+        if (err)
+            console.log('error:', err);
+        else
+            console.log(JSON.stringify(response, null, 2));
+        return response.json(test);
+    });
 
-    // nlu.analyze({
-    //     'html': string, // Buffer or String
-    //     'features': {
-    //         'concepts': {},
-    //         'keywords': {},
-    //         'emotion': {}
-    //     }
-    // }, function(err, response) {
-    //     if (err)
-    //         console.log('error:', err);
-    //     else
-    //         console.log(JSON.stringify(response, null, 2));
-    //    return response.emotion;
-    // });
-}
+});
+
+
 
 module.exports = api;
