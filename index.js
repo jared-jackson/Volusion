763c6fd6183be67b2f1a78774df656e07d6bc1b4
jared-jackson <lanejackson4@hotmail.com>
2017-09-09 14:54:28 -0500

    consultWatson code changes

--- index.js
+++ index.js
@@ -21,23 +21,24 @@ function consultWatson(string) {
         "version_date": NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
     });
 
-    nlu.analyze({
-        'html': string, // Buffer or String
-        'features': {
-            'concepts': {},
-            'keywords': {},
-            'emotion': {}
-        }
-    }, function (err, response) {
-        //TODO REFACTOR THIS. IT SUCKS
-        if (err) {
-            console.log('error:', err);
-            return err;
-        } else {
-            //return JSON.stringify(response, null, 2);
-            //return response;
-        }
-    });
+    return string;
+    // nlu.analyze({
+    //     'html': string, // Buffer or String
+    //     'features': {
+    //         'concepts': {},
+    //         'keywords': {},
+    //         'emotion': {}
+    //     }
+    // }, function (err, response) {
+    //     //TODO REFACTOR THIS. IT SUCKS
+    //     if (err) {
+    //         console.log('error:', err);
+    //         return err;
+    //     } else {
+    //         //return JSON.stringify(response, null, 2);
+    //         //return response;
+    //     }
+    // });
 };
 
 module.exports = api;
