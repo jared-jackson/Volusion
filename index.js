7c3850cbdb09331d648d095e2728d75c72af8f43
jared-jackson <lanejackson4@hotmail.com>
2017-09-09 13:59:23 -0500

    added IBM Watson NLP package, initial function for consulting Watson for insight

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+'use strict';
+var ApiBuilder = require('claudia-api-builder');
+var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 
-var ApiBuilder = require('claudia-api-builder'),  
-  
-
-api = new ApiBuilder();
+var api = new ApiBuilder();
 
 
 api.get('/', function () {  
@@ -13,8 +13,27 @@ api.get('/', function () {
 
 
 
+function consultWatson(string) {
+    var nlu = new NaturalLanguageUnderstandingV1({
+        "url": "https://gateway.watsonplatform.net/natural-language-understanding/api",
+        "username": "f1a68365-b09e-4ad1-b17e-52a0d5f80f4c",
+        "password": "JBAtZWNXWqy6"
+    });
 
-
-
+    nlu.analyze({
+        'html': string, // Buffer or String
+        'features': {
+            'concepts': {},
+            'keywords': {},
+            'emotion': {}
+        }
+    }, function(err, response) {
+        if (err)
+            console.log('error:', err);
+        else
+            console.log(JSON.stringify(response, null, 2));
+        console.log(response.emotion);
+    });
+};
 
 module.exports = api;
