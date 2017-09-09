28dbc811c942dcdf6ef925ccaad9837c91584dc1
jared-jackson <lanejackson4@hotmail.com>
2017-09-09 16:25:20 -0500

    consultWatson code changes

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
 'use strict';
 var ApiBuilder = require('claudia-api-builder');
+var async = require('async');
 var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 
 var api = new ApiBuilder();
 
 
-api.get('/', function () {
+api.get('/', function (req, res) {
 
-    var test_string = "<div><h3>THIS IS A SUPER AWESOME STRING WE ARE GOING TO BE EXAMINING</h3>";
+    var string = " THIS IS OUR AMAZING TEXT STRING";
 
-    var nlu = new NaturalLanguageUnderstandingV1({
-        username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
-        password: 'JBAtZWNXWqy6',
-        version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
-    });
+    async.waterfall(
+        [
+            function (callback) {
+                var nlu = new NaturalLanguageUnderstandingV1({
+                    username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
+                    password: 'JBAtZWNXWqy6',
+                    version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
+                });
+                var test = {};
+                nlu.analyze({
+                    'html': string, // Buffer or String
+                    'features': {
+                        'concepts': {},
+                        'keywords': {},
+                        'emotion': {}
+                    }
+                }, function (err, response) {
+                    if (err) {
+                        console.log('error:', err);
+                    } else {
+                        test = response.emotion.document.emotion;
+                        callback(null, test, 'There was an error processing the language text.');
+                    }
 
-    nlu.analyze({
-        'html': test_string, // Buffer or String
-        'features': {
-            'concepts': {},
-            'keywords': {},
-            'emotion': {}
+                });
+            }
+
+        ],
+        function (err, insights) {
+            res.status(200).send(insights);
         }
-    }, function(err, response) {
-        var test = response.emotion;
-        if (err)
-            console.log('error:', err);
-        else
-            console.log(JSON.stringify(response, null, 2));
-        return response.json(test);
-    });
+    );
+
 
 });
-
 
 
 module.exports = api;
