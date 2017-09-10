c87befe9e79066dcd70b4a2aecafb419a5944fe1
jared-jackson <lanejackson4@hotmail.com>
2017-09-10 11:29:58 -0500

    consultWatson code changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var ApiBuilder = require('claudia-api-builder');
-var async = require('async');
+var denodeify = require('denodeify');
 var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 
 var api = new ApiBuilder();
@@ -10,38 +10,20 @@ api.get('/', function (req, res) {
     var string = " THIS IS OUR AMAZING TEXT STRING";
 
 
-    var watsonInsight = function (params, cb) {
-        async.waterfall(
-            [
-                function (callback) {
-                    var nlu = new NaturalLanguageUnderstandingV1({
-                        username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
-                        password: 'JBAtZWNXWqy6',
-                        version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
-                    });
-                    var test = {};
-                    nlu.analyze({
-                        'html': string, // Buffer or String
-                        'features': {
-                            'concepts': {},
-                            'keywords': {},
-                            'emotion': {}
-                        }
-                    }, function (err, response) {
-                        if (err) {
-                            console.log('error:', err);
-                        } else {
-                            test = response.emotion.document.emotion;
-                            callback(null, test, 'There was an error processing the language text.');
-                        }
+    var exec = denodeify(function(err, req, res) {
+        return "here is an asynchronous return string";
+    });
 
-                    });
-                }
+    exec().then(function(results) {
 
-            ], cb);
-    };
+            return results;
 
-    return watsonInsight;
+        });
+
+
+
+
+
 
 }, {success: {contentType: 'application/json'}});
 
