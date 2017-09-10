06028bd07ba799b169615b19fe18afd403784d0d
jared-jackson <lanejackson4@hotmail.com>
2017-09-10 15:10:38 -0500

    consultWatson code changes

--- index.js
+++ index.js
@@ -11,31 +11,44 @@ api.get('/', function (req) {
 
     var analyze_url = req.queryString.url;
 
-    var analyze_content = "";
+    return request(analyze_url, function (error, response, html) {
+        if (!error) {
+            var $ = cheerio.load(html);
+            var test = "";
+            $('.post-content').filter(function () {
+                var data = $(this);
+                var article_content = data.parent().children().text().trim();
+                test = createTextVersion(article_content);
+            });
+            var analyze_content = test.toString();
+            new RSVP.Promise(function (resolve, reject) {
+                var nlu = new NaturalLanguageUnderstandingV1({
+                    username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
+                    password: 'JBAtZWNXWqy6',
+                    version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
+                });
 
-    return new RSVP.Promise(function (resolve, reject) {
-        var nlu = new NaturalLanguageUnderstandingV1({
-            username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
-            password: 'JBAtZWNXWqy6',
-            version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
-        });
-        var test = {};
-        nlu.analyze({
-            'html': analyze_content,
-            'features': {
-                'concepts': {},
-                'keywords': {},
-                'emotion': {}
-            }
-        }, function (err, response) {
-            if (err) {
-                reject(err);
-            } else {
-                test = response.emotion.document.emotion;
-                resolve(test);
-            }
-        });
+                nlu.analyze({
+                    'html': analyze_content,
+                    'features': {
+                        'concepts': {},
+                        'keywords': {},
+                        'emotion': {}
+                    }
+                }, function (err, response) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        test = response.emotion.document.emotion;
+                        resolve(test);
+                    }
+                });
+            });
+        }
     });
+
+
+
 }, {success: {contentType: 'application/json'}});
 
 module.exports = api;
