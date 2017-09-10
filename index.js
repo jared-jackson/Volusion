96df5ebca7076c7fedcd4df81a8d6730e4915bdb
jared-jackson <lanejackson4@hotmail.com>
2017-09-10 15:58:03 -0500

    consultWatson code changes

--- index.js
+++ index.js
@@ -14,83 +14,84 @@ api.get('/', function (req) {
 
 //Aproach 1
 
-    // return new RSVP.Promise(function (resolve, reject) {
-    //     request(analyze_url, function (error, response, html) {
-    //         if (!error) {
-    //             var $ = cheerio.load(html);
-    //             var test = "";
-    //             $('.post-content').filter(function () {
-    //                 var data = $(this);
-    //                 var article_content = data.parent().children().text().trim();
-    //                 test = createTextVersion(article_content);
-    //             });
-    //             var nlu = new NaturalLanguageUnderstandingV1({
-    //                 username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
-    //                 password: 'JBAtZWNXWqy6',
-    //                 version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
-    //             });
-    //             var analyze_text = test.toString();
-    //
-    //
-    //             resolve(analyze_text);
-    //
-    //             // nlu.analyze({
-    //             //     'html': analyze_text,
-    //             //     'features': {
-    //             //         'concepts': {},
-    //             //         'keywords': {},
-    //             //         'emotion': {}
-    //             //     }
-    //             // }, function (err, response) {
-    //             //     if (err) {
-    //             //         reject(err);
-    //             //     } else {
-    //             //         test = response.emotion.document.emotion;
-    //             //         resolve(test);
-    //             //     }
-    //             // });
-    //         }
-    //     })
-    // });
-
-
-// Approach 2
-    request(analyze_url, function (error, response, html) {
-        console.log(html);
-        if (!error) {
-            var $ = cheerio.load(html);
-            var test = "";
-            $('.post-content').filter(function () {
-                var data = $(this);
-                var article_content = data.parent().children().text().trim();
-                test = createTextVersion(article_content);
-            });
-            var analyze_content = test.toString();
-            console.log(analyze_content);
-            return new RSVP.Promise(function (resolve, reject) {
+    return new RSVP.Promise(function (resolve, reject) {
+        request(analyze_url, function (error, response, html) {
+            console.log(analyze_url, "THIS IS THE URL");
+            if (!error) {
+                var $ = cheerio.load(html);
+                var test = "";
+                $('.post-content').filter(function () {
+                    var data = $(this);
+                    var article_content = data.parent().children().text().trim();
+                    test = createTextVersion(article_content);
+                });
                 var nlu = new NaturalLanguageUnderstandingV1({
                     username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
                     password: 'JBAtZWNXWqy6',
                     version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
                 });
-                nlu.analyze({
-                    'html': analyze_content,
-                    'features': {
-                        'concepts': {},
-                        'keywords': {},
-                        'emotion': {}
-                    }
-                }, function (err, response) {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        test = response.emotion.document.emotion;
-                        resolve(test);
-                    }
-                });
-            });
-        }
+                var analyze_text = test.toString();
+
+
+                resolve(analyze_text);
+
+                // nlu.analyze({
+                //     'html': analyze_text,
+                //     'features': {
+                //         'concepts': {},
+                //         'keywords': {},
+                //         'emotion': {}
+                //     }
+                // }, function (err, response) {
+                //     if (err) {
+                //         reject(err);
+                //     } else {
+                //         test = response.emotion.document.emotion;
+                //         resolve(test);
+                //     }
+                // });
+            }
+        })
     });
+
+
+// Approach 2
+//     request(analyze_url, function (error, response, html) {
+//         console.log(html);
+//         if (!error) {
+//             var $ = cheerio.load(html);
+//             var test = "";
+//             $('.post-content').filter(function () {
+//                 var data = $(this);
+//                 var article_content = data.parent().children().text().trim();
+//                 test = createTextVersion(article_content);
+//             });
+//             var analyze_content = test.toString();
+//             console.log(analyze_content);
+//             return new RSVP.Promise(function (resolve, reject) {
+//                 var nlu = new NaturalLanguageUnderstandingV1({
+//                     username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
+//                     password: 'JBAtZWNXWqy6',
+//                     version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
+//                 });
+//                 nlu.analyze({
+//                     'html': analyze_content,
+//                     'features': {
+//                         'concepts': {},
+//                         'keywords': {},
+//                         'emotion': {}
+//                     }
+//                 }, function (err, response) {
+//                     if (err) {
+//                         reject(err);
+//                     } else {
+//                         test = response.emotion.document.emotion;
+//                         resolve(test);
+//                     }
+//                 });
+//             });
+//         }
+//     });
 
 //WORKING
 //     var analyze_content = "THIS IS OUR SUPER AWESOME STRING";
