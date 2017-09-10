f6650b8c850b0a741054a4238f5e852839a87e68
jared-jackson <lanejackson4@hotmail.com>
2017-09-10 11:44:52 -0500

    consultWatson code changes

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 var ApiBuilder = require('claudia-api-builder');
-var denodeify = require('denodeify');
+var RSVP = require('rsvp');
 var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 
 var api = new ApiBuilder();
 
 
-api.get('/', function (req, res) {
+api.get('/', function () {
     'use strict';
-    var string = " THIS IS OUR AMAZING TEXT STRING";
+    var string = 'THIS IS OUR AMAZING TEST STRING';
 
-
-    var exec = denodeify(function(err, req, res) {
-        return "here is an asynchronous return string";
-    });
-
-    exec().then(function(results) {
-
-            return results;
-
+    function randomQuote() {
+        return new RSVP.Promise(function(resolve, reject) {
+            var nlu = new NaturalLanguageUnderstandingV1({
+                username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
+                password: 'JBAtZWNXWqy6',
+                version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
+            });
+            var test = {};
+            nlu.analyze({
+                'html': string, // Buffer or String
+                'features': {
+                    'concepts': {},
+                    'keywords': {},
+                    'emotion': {}
+                }
+            }, function (err, response) {
+                if (err) {
+                    reject(err);
+                } else {
+                    test = response.emotion.document.emotion;
+                    resolve(test);
+                }
+            });
         });
+    }
+
+    return randomQuote().then(console.log);
 
 
 
