f168e7be3fd96fc7dc932ff013503c5c28a6e013
jared-jackson <lanejackson4@hotmail.com>
2017-09-10 15:14:46 -0500

    consultWatson code changes

--- index.js
+++ index.js
@@ -9,27 +9,28 @@ var api = new ApiBuilder();
 
 api.get('/', function (req) {
 
+
+
     var analyze_url = req.queryString.url;
 
-    return request(analyze_url, function (error, response, html) {
-        if (!error) {
-            var $ = cheerio.load(html);
-            var test = "";
-            $('.post-content').filter(function () {
-                var data = $(this);
-                var article_content = data.parent().children().text().trim();
-                test = createTextVersion(article_content);
-            });
-            var analyze_content = test.toString();
-            new RSVP.Promise(function (resolve, reject) {
+    return new RSVP.Promise(function (resolve, reject) {
+        request(analyze_url, function (error, response, html) {
+            if (!error) {
+                var $ = cheerio.load(html);
+                var test = "";
+                $('.post-content').filter(function () {
+                    var data = $(this);
+                    var article_content = data.parent().children().text().trim();
+                    test = createTextVersion(article_content);
+                });
                 var nlu = new NaturalLanguageUnderstandingV1({
                     username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
                     password: 'JBAtZWNXWqy6',
                     version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
                 });
-
+                var analyze_text = test.toString();
                 nlu.analyze({
-                    'html': analyze_content,
+                    'html': analyze_text,
                     'features': {
                         'concepts': {},
                         'keywords': {},
@@ -43,9 +44,10 @@ api.get('/', function (req) {
                         resolve(test);
                     }
                 });
-            });
-        }
-    });
+            }
+        })
+
+
 
 
 
