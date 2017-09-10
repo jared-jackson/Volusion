836abc3b47f5dbdf43ad4118b5529d8d25eaab6e
jared-jackson <lanejackson4@hotmail.com>
2017-09-10 14:01:46 -0500

    consultWatson code changes

--- index.js
+++ index.js
@@ -8,29 +8,33 @@ var api = new ApiBuilder();
 api.get('/', function (req) {
     var string = 'THIS IS OUR AMAZING TEST STRING';
 
-    return new RSVP.Promise(function (resolve, reject) {
-        var nlu = new NaturalLanguageUnderstandingV1({
-            username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
-            password: 'JBAtZWNXWqy6',
-            version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
-        });
-        var test = {};
-        nlu.analyze({
-            'html': string,
-            'features': {
-                'concepts': {},
-                'keywords': {},
-                'emotion': {}
-            }
-        }, function (err, response) {
-            if (err) {
-                reject(err);
-            } else {
-                test = response.emotion.document.emotion;
-                resolve(test);
-            }
-        });
-    });
+    // return new RSVP.Promise(function (resolve, reject) {
+    //     var nlu = new NaturalLanguageUnderstandingV1({
+    //         username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
+    //         password: 'JBAtZWNXWqy6',
+    //         version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
+    //     });
+    //     var test = {};
+    //     nlu.analyze({
+    //         'html': string,
+    //         'features': {
+    //             'concepts': {},
+    //             'keywords': {},
+    //             'emotion': {}
+    //         }
+    //     }, function (err, response) {
+    //         if (err) {
+    //             reject(err);
+    //         } else {
+    //             test = response.emotion.document.emotion;
+    //             resolve(test);
+    //         }
+    //     });
+    // });
+
+    return req.queryString.name
+
+
 }, {success: {contentType: 'application/json'}});
 
 
