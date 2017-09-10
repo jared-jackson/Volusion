e27c3ee78f715978f4af2761198bff82fe372de2
jared-jackson <lanejackson4@hotmail.com>
2017-09-10 11:17:30 -0500

    consultWatson code changes

--- index.js
+++ index.js
@@ -10,39 +10,40 @@ api.get('/', function (req, res) {
     var string = " THIS IS OUR AMAZING TEXT STRING";
 
 
-    async.waterfall(
-        [
-            function (callback) {
-                var nlu = new NaturalLanguageUnderstandingV1({
-                    username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
-                    password: 'JBAtZWNXWqy6',
-                    version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
-                });
-                var test = {};
-                nlu.analyze({
-                    'html': string, // Buffer or String
-                    'features': {
-                        'concepts': {},
-                        'keywords': {},
-                        'emotion': {}
-                    }
-                }, function (err, response) {
-                    if (err) {
-                        console.log('error:', err);
-                    } else {
-                        test = response.emotion.document.emotion;
-                        callback(null, test, 'There was an error processing the language text.');
-                    }
+    var watsonInsight = function (params, cb) {
+        async.waterfall(
+            [
+                function (callback) {
+                    var nlu = new NaturalLanguageUnderstandingV1({
+                        username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
+                        password: 'JBAtZWNXWqy6',
+                        version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
+                    });
+                    var test = {};
+                    nlu.analyze({
+                        'html': string, // Buffer or String
+                        'features': {
+                            'concepts': {},
+                            'keywords': {},
+                            'emotion': {}
+                        }
+                    }, function (err, response) {
+                        if (err) {
+                            console.log('error:', err);
+                        } else {
+                            test = response.emotion.document.emotion;
+                            callback(null, test, 'There was an error processing the language text.');
+                        }
 
-                });
-            }
+                    });
+                }
 
-        ],
-        function (err, insights) {
-           return insights;
-        }
-    );
-},{ success: { contentType: 'application/json'}});
+            ], cb);
+    };
+
+    return watsonInsight;
+
+}, {success: {contentType: 'application/json'}});
 
 
 module.exports = api;
