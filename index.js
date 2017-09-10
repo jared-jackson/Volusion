41ea2878a4f9f8ba652ef36ae5d8339d06fdf01d
jared-jackson <lanejackson4@hotmail.com>
2017-09-10 15:18:04 -0500

    consultWatson code changes

--- index.js
+++ index.js
@@ -11,40 +11,73 @@ api.get('/', function (req) {
 
     var analyze_url = req.queryString.url;
 
+    // return new RSVP.Promise(function (resolve, reject) {
+    //     request(analyze_url, function (error, response, html) {
+    //         if (!error) {
+    //             var $ = cheerio.load(html);
+    //             var test = "";
+    //             $('.post-content').filter(function () {
+    //                 var data = $(this);
+    //                 var article_content = data.parent().children().text().trim();
+    //                 test = createTextVersion(article_content);
+    //             });
+    //             var nlu = new NaturalLanguageUnderstandingV1({
+    //                 username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
+    //                 password: 'JBAtZWNXWqy6',
+    //                 version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
+    //             });
+    //             var analyze_text = test.toString();
+    //             nlu.analyze({
+    //                 'html': analyze_text,
+    //                 'features': {
+    //                     'concepts': {},
+    //                     'keywords': {},
+    //                     'emotion': {}
+    //                 }
+    //             }, function (err, response) {
+    //                 if (err) {
+    //                     reject(err);
+    //                 } else {
+    //                     test = response.emotion.document.emotion;
+    //                     resolve(test);
+    //                 }
+    //             });
+    //         }
+    //     })
+    // });
+
+
+    var analyze_content = "This is an awesome test string";
+
     return new RSVP.Promise(function (resolve, reject) {
-        request(analyze_url, function (error, response, html) {
-            if (!error) {
-                var $ = cheerio.load(html);
-                var test = "";
-                $('.post-content').filter(function () {
-                    var data = $(this);
-                    var article_content = data.parent().children().text().trim();
-                    test = createTextVersion(article_content);
-                });
-                var nlu = new NaturalLanguageUnderstandingV1({
-                    username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
-                    password: 'JBAtZWNXWqy6',
-                    version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
-                });
-                var analyze_text = test.toString();
-                nlu.analyze({
-                    'html': analyze_text,
-                    'features': {
-                        'concepts': {},
-                        'keywords': {},
-                        'emotion': {}
-                    }
-                }, function (err, response) {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        test = response.emotion.document.emotion;
-                        resolve(test);
-                    }
-                });
+        var nlu = new NaturalLanguageUnderstandingV1({
+            username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
+            password: 'JBAtZWNXWqy6',
+            version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
+        });
+        var test = {};
+        nlu.analyze({
+            'html': analyze_content,
+            'features': {
+                'concepts': {},
+                'keywords': {},
+                'emotion': {}
             }
-        })
+        }, function (err, response) {
+            if (err) {
+                reject(err);
+            } else {
+                test = response.emotion.document.emotion;
+                resolve(test);
+            }
+        });
     });
+
+
+
+
+
+
 
 
 }, {success: {contentType: 'application/json'}});
