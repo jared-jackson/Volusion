b2c2cb7463e16b2b6bb0be74c627238cd60410bc
jared-jackson <lanejackson4@hotmail.com>
2017-09-10 11:54:12 -0500

    consultWatson code changes

--- index.js
+++ index.js
@@ -1,44 +1,36 @@
+'use strict';
 var ApiBuilder = require('claudia-api-builder');
-var RSVP = require('rsvp');
 var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+var RSVP = require('rsvp');
 
 var api = new ApiBuilder();
 
-
-api.get('/', function () {
-    'use strict';
+api.get('/', function (req) {
     var string = 'THIS IS OUR AMAZING TEST STRING';
 
-
-        return new RSVP.Promise(function(resolve, reject) {
-            var nlu = new NaturalLanguageUnderstandingV1({
-                username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
-                password: 'JBAtZWNXWqy6',
-                version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
-            });
-            var test = {};
-            nlu.analyze({
-                'html': string, // Buffer or String
-                'features': {
-                    'concepts': {},
-                    'keywords': {},
-                    'emotion': {}
-                }
-            }, function (err, response) {
-                if (err) {
-                    reject(err);
-                } else {
-                    test = response.emotion.document.emotion;
-                    resolve(test);
-                }
-            });
+    return new RSVP.Promise(function (resolve, reject) {
+        var nlu = new NaturalLanguageUnderstandingV1({
+            username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
+            password: 'JBAtZWNXWqy6',
+            version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
         });
-
-
-
-
-
-
+        var test = {};
+        nlu.analyze({
+            'html': string,
+            'features': {
+                'concepts': {},
+                'keywords': {},
+                'emotion': {}
+            }
+        }, function (err, response) {
+            if (err) {
+                reject(err);
+            } else {
+                test = response.emotion.document.emotion;
+                resolve(test);
+            }
+        });
+    });
 }, {success: {contentType: 'application/json'}});
 
 
