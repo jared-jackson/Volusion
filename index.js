c3ffede347a9455dfd3e7e1e6af1e8d3fc9fa0b1
jared-jackson <lanejackson4@hotmail.com>
2017-09-10 23:04:01 -0500

    Finalized Lambda function for consulting Mr. Watson

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var cheerio = require('cheerio');
 var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 var request = require('request');
 var RSVP = require('rsvp');
+var rp = require('request-promise');
 
 var api = new ApiBuilder();
 
@@ -55,72 +56,109 @@ var api = new ApiBuilder();
 // }, {success: {contentType: 'application/json'}});
 
 
-//Approach 2
+//Approach 2 -- Socket hang up up when request module takes too long
+// api.get('/', function (req) {
+//     var analyze_url = req.queryString.url;
+//     console.log("initial api call to url: " + analyze_url);
+//     var promise = new RSVP.Promise(function (resolve, reject) {
+//         request(analyze_url, function (error, response, html) {
+//             console.log("in request");
+//             var article_content = "";
+//             if (!error) {
+//                 var $ = cheerio.load(html);
+//                 $('.post-content').filter(function () {
+//                     var data = $(this);
+//                     article_content = data.parent().children().text().trim();
+//                     console.log("trying to resolve article content" + article_content);
+//                     resolve(article_content);
+//                 });
+//             } else {
+//                 console.log("REJECTING" + error);  // socket hang up when request takes too long
+//                 reject(error);
+//             }
+//         });
+//     });
+//     promise.then(function (value) {
+//         console.log("content outside promise" + value);
+//         return new RSVP.Promise(function (resolve, reject) {
+//             var watson_response = "";
+//             var article_content = value.toString();
+//
+//             console.log("CONTENT INSIDE PROMISE", article_content);
+//
+//             var nlu = new NaturalLanguageUnderstandingV1({
+//                 username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
+//                 password: 'JBAtZWNXWqy6',
+//                 version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
+//             });
+//             var analyze_text = article_content.toString();
+//             console.log("ANALYZING TEXT" + analyze_text);
+//             //I think nlu.analyze is finishing before request completes its call to get the html
+//             // nlu.analyze({
+//             //     'html': analyze_text,
+//             //     'features': {
+//             //         'concepts': {},
+//             //         'keywords': {},
+//             //         'emotion': {}
+//             //     }
+//             // }, function (err, response) {
+//             //     console.log(err, "The interal server error we're getting")
+//             //     if (err) {
+//             //         reject(err);
+//             //     } else {
+//             //         watson_response = response.emotion.document.emotion;
+//             //         console.log("RESPONSE " + watson_response);
+//             //         resolve(watson_response);
+//             //     }
+//             // });
+//         });
+//
+//     }).catch(function (error) {
+//         // failure
+//     });
+// }, {success: {contentType: 'application/json'}});
 
+
+//Approach 3
 api.get('/', function (req) {
     var analyze_url = req.queryString.url;
     console.log("initial api call to url: " + analyze_url);
-    var promise = new RSVP.Promise(function (resolve, reject) {
-        request(analyze_url, function (error, response, html) {
-            console.log("in request");
-            var article_content = "";
-            if (!error) {
-                var $ = cheerio.load(html);
-                $('.post-content').filter(function () {
-                    var data = $(this);
-                    article_content = data.parent().children().text().trim();
-                    console.log("trying to resolve article content" + article_content);
-                    resolve(article_content);
-                });
+
+    rp(analyze_url).then(function (htmlString) {
+        var article_content = "";
+        var watson_response;
+        var $ = cheerio.load(htmlString);
+        $('.post-content').filter(function () {
+            var data = $(this);
+            article_content = data.parent().children().text().trim();
+        });
+
+        var nlu = new NaturalLanguageUnderstandingV1({
+            username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
+            password: 'JBAtZWNXWqy6',
+            version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
+        });
+        var analyze_text = article_content.toString();
+        //I think nlu.analyze is finishing before request completes its call to get the html
+        nlu.analyze({
+            'html': analyze_text,
+            'features': {
+                'concepts': {},
+                'keywords': {},
+                'emotion': {}
+            }
+        }, function (err, response) {
+            if (err) {
             } else {
-                console.log("REJECTING" + error);
-                reject(error);
+                watson_response = response.emotion.document.emotion;
+                console.log("WATSON HAS SPOKEN " + watson_response);
+                return watson_response;
             }
         });
+
+    }).catch(function (err) {
+        // Crawling failed...
     });
-    promise.then(function (value) {
-        console.log("content outside promise" + value);
-        return new RSVP.Promise(function (resolve, reject) {
-            var watson_response = "";
-            var article_content = value.toString();
-
-            console.log("CONTENT INSIDE PROMISE", article_content);
-
-            var nlu = new NaturalLanguageUnderstandingV1({
-                username: 'f1a68365-b09e-4ad1-b17e-52a0d5f80f4c',
-                password: 'JBAtZWNXWqy6',
-                version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
-            });
-            var analyze_text = article_content.toString();
-            console.log("ANALYZING TEXT" + analyze_text);
-            //I think nlu.analyze is finishing before request completes its call to get the html
-            // nlu.analyze({
-            //     'html': analyze_text,
-            //     'features': {
-            //         'concepts': {},
-            //         'keywords': {},
-            //         'emotion': {}
-            //     }
-            // }, function (err, response) {
-            //     console.log(err, "The interal server error we're getting")
-            //     if (err) {
-            //         reject(err);
-            //     } else {
-            //         watson_response = response.emotion.document.emotion;
-            //         console.log("RESPONSE " + watson_response);
-            //         resolve(watson_response);
-            //     }
-            // });
-        });
-
-    }).catch(function (error) {
-        // failure
-    });
-
-
-
-
-
 
 
 
@@ -128,6 +166,7 @@ api.get('/', function (req) {
 
 
 }, {success: {contentType: 'application/json'}});
+
 
 
 module.exports = api;
